6eae99320460700ffd44ca055be89c749e15bc7a
jalilpablo <jalilpablo@gmail.com>
2022-03-21 22:02:51 -0300

    sigo resolviendo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -160,6 +160,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero!==0 && numero!==1){
+   for (let i = 2 ; i<numero ;i++){
+     if (numero%i !== 0){
+       return 'true';
+     }
+      return 'falso';
+   }
+  }
+  return 'falso'
 }
 
 function esVerdadero(valor){
